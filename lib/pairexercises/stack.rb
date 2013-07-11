9db001c412b891c23c7379c6a04329748a4afc0c
Sam Joseph <tansaku@gmail.com>
2013-07-11 13:46:56 +0100

    TDD pairing session results

--- lib/pairexercises/stack.rb
+++ lib/pairexercises/stack.rb
@@ -1,4 +1,13 @@
 module PairExercises
   class Stack
+      def initialize
+         @store = []
+      end
+      def push(item)
+         @store.push item
+      end
+      def pop
+         @store.pop
+      end      
   end
 end
--- spec/pairexercises/stack_spec.rb
+++ spec/pairexercises/stack_spec.rb
@@ -3,7 +3,15 @@ require 'spec_helper'
 module PairExercises
 	describe Stack do 
 		describe "#pop" do
-			
+			it "should pop off the last thing pushed on to it" do
+				stack = Stack.new
+				(1..10).each do |n|
+                    stack.push(n)
+                end    
+                (10..1).each do |n|
+                    expect(stack.pop).to eq(n)
+                end
+		    end
 		end
 	end
-end
+end 
